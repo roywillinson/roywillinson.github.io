611d3825a7bc15f4b935c7338f13a9649ab25d5f
Roy Willynson <roywillinson@gmail.com>
2020-04-27 19:29:45 -0400

    Arreglando cosas

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,29 +1,47 @@
+//Variables
+let navbar = document.getElementById('head-navbar');
+let footer = document.getElementById('main-footer');
+
+//Funcion Ajustar margen del footer
+function ajustarMargen() {
+
+    //Alto de Footer
+    let footerHeight = footer.clientHeight;
+    //Elemento Opciones
+    let op = document.getElementById('opciones');
+    //Estilo del Margen
+    let marginTopOp = window.getComputedStyle(op, null)
+        .getPropertyValue('margin-top');
+    //Margen AJustado
+    let sizeAjust = (footerHeight + parseInt(marginTopOp.replace('px', '')));
+
+    document.body.style.marginBottom = sizeAjust + 'px';
+}
+
+
+
 //Si la ventana carga
-$(document).ready(function () {
+window.onload = () => {
+
     //Agrega el efecto change scroll
-    $(window).scroll(function () {
-        if ($(document).scrollTop() > 150) {
-            $('.navbar').addClass('color-change');
+    window.onscroll = () => {
+
+        if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
+
+            navbar.classList.add('color-change');
+            navbar.classList.add('shadow');
+
         } else {
-            $('.navbar').removeClass('color-change');
+
+            navbar.classList.remove('color-change');
+            navbar.classList.remove('shadow');
         }
-    });
 
-    //Funcion Ajustar margen del footer
-    function ajustarMargen() {
-        //Por primera vez
-        //Variable de alto de footer
-        let footerHeight = $('footer').height();
-        let margT = $('#opciones').css('margin-top');
-        //Margen AJustado
-        let sizeAjust = (footerHeight + parseInt(margT.replace('px', '')));
-        $('body').css("margin-bottom", `${sizeAjust}px`);
-    }
-
+    };
 
     ajustarMargen();
 
     //Adaptar footer
-    $(window).resize(ajustarMargen);
+    window.onresize = ajustarMargen;
 
-});
+};
